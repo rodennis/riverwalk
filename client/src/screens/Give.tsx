f9507ad83d0522be4fa430fe421c35c42afe7544
rodennis <rev.rltodd@gmail.com>
2022-05-29 17:01:05 -0500

    create footer, and header files, create neccessary screens files

---
+++ client/src/screens/Give.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Give = () => {
+  return (
+    <div>Give</div>
+  )
+}
---
+++ client/src/screens/Media.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Media = () => {
+  return (
+    <div>Media</div>
+  )
+}
